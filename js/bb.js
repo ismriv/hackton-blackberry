7b11f407d0df4b5f02f1d495c164d6eefa0b8779
Leonardo Balter <leonardo.balter@gmail.com>
2012-09-22 15:56:34 -0300

    modificacoes leo rapido hackathon

--- js/bb.js
+++ js/bb.js
@@ -4,14 +4,69 @@ jQuery( function( $ ) {
 
     window.tripCalc = tripCalc;
 
-    tripCalc.editItem = function() {
-        if ( arguments[0] && arguments[0].preventDefault ) {
-            arguments[0].preventDefault();
-        }
+    tripCalc.loadPage = function() {
+        var itemList = $( '#item-list' ),
+            itemTemplate = $( '#item-template'),
+            ls = window.localStorage,
+            i, l;
 
-        
+
     };
 
-    doc.on( 'click', '.calc-item.editable', tripCalc.editItem );
-    doc.on( 'click', '.item-action', tripCalc.editDone );
+    tripCalc.editDone = function( ev ) {
+        if ( ev && ev.preventDefault ) {
+            ev.preventDefault();
+            ev.stopBubbling();
+        }
+
+        $( '.calc-item .editable' ).each( function() {
+            $( this ).removeAttr( 'contenteditable' );
+        });
+
+        $( this ).addClass( 'editable' );
+
+        tripCalc.saveItem( this );
+    }
+
+    tripCalc.editItem = function( ev ) {
+        if ( ev && ev.preventDefault ) {
+            ev.preventDefault();
+        }
+
+        var self = $( this ),
+            editable = self.find( '.editable' );
+
+        tripCalc.editDone();
+
+        editable.each( function() {
+            $( this ).attr( 'contenteditable', '' );
+        });
+    };
+
+    tripCalc.saveItem = function( elem ) {
+        if ( !elem ) {
+            return;
+        }
+
+        var self = $( elem ),
+            itemId = self.data( 'id' ) || Date.now(),
+            qtt = self.find( '.qtt' ).text(),
+            value = self.find( '.value' ).text(),
+            desc = self.find( '.description' ).text(),
+            ls = window.localStorage;
+
+        self.data( 'id', itemId );
+
+        ls['tripItems'] = ls['tripItems'] || [];
+
+        ls['tripItems'].itemId = {
+            qtt : qtt,
+            value : value,
+            desc : desc,
+            lastUpdated : Date.now()
+        };
+    };
+
+    doc.on( 'click touchDown touch', '.calc-item.editable', tripCalc.editItem );
+    doc.on( 'click touchDown touch', '.item-action', tripCalc.editDone );
 });
